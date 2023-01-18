180f81cd77564a3baafac5abf98fe50ba75cc5cc
Bruna Lika <brunalika@msn.com>
2023-01-18 17:19:40 -0300

    database migration separate project

---
+++ DatabaseProject/ChampionContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Championship_Login_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseProject
+{
+	public class ChampionContext : DbContext
+    {
+		public ChampionContext(DbContextOptions options) : base(options)
+        {
+		}
+
+        #region SystemTables
+        public DbSet<User> Users { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Championship> Championships { get; set; }
+        public DbSet<Competition> Competitions { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        #endregion
+    }
+}
+
---
+++ DatabaseProject/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace DatabaseProject
+{
+	public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ChampionContext>
+    {
+		public DesignTimeDbContextFactory()
+		{
+		}
+
+        public ChampionContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var builder = new DbContextOptionsBuilder<ChampionContext>();
+            var connectionString = configuration.GetConnectionString("ChampionConnection");
+            builder.UseSqlServer(connectionString);
+            return new ChampionContext(builder.Options);
+        }
+    }
+}
+
