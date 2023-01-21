5bad3ac31e3886d9119a5163b741d67ad8a55f22
Proxyturn <william.a.finardi@gmail.com>
2023-01-21 15:57:42 -0300

    Adding TeamLeader aspect

--- DatabaseProject/Migrations/20230121185513_TeamLeader.Designer.cs
+++ DatabaseProject/Migrations/20230121185513_TeamLeader.Designer.cs
@@ -4,6 +4,7 @@ using DatabaseProject;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DatabaseProject.Migrations
 {
     [DbContext(typeof(ChampionContext))]
-    partial class ChampionContextModelSnapshot : ModelSnapshot
+    [Migration("20230121185513_TeamLeader")]
+    partial class TeamLeader
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- DatabaseProject/Models/Team.cs
+++ DatabaseProject/Models/Team.cs
@@ -5,5 +5,6 @@
         public Guid Id { get; set; }
         public string? Name { get; set; }
         public Guid IdChampionship { get; set; }
+        public Guid IdLeader { get; set; }
     }
 }
