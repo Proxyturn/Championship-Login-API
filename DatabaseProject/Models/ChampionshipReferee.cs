add9093d956ca8b3f69d8583d60c03497f7da380
Bruna Lika <brunalika@msn.com>
2023-04-14 16:21:53 -0300

    comments

--- DatabaseProject/Models/ChampionshipReferee.cs
+++ DatabaseProject/Models/ChampionshipReferee.cs
@@ -3,7 +3,7 @@
 	public class ChampionshipReferee
 	{
         public Guid Id { get; set; }
-        public Guid UserId { get; set; }
+        public Guid UserId { get; set; } //RefereeId
         public Guid ChampionshipId { get; set; }
     }
 }
