63464f880b13d3894f443abf0fd495dc508d1939
Bruna Lika <brunalika@msn.com>
2023-05-02 23:13:21 -0300

    migration - new columns in match

---
+++ DatabaseProject/Migrations/20230503020711_AlterMatchFinishDateWinner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterMatchFinishDateWinner : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FinishDate",
+                table: "Matchs",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "IdWinner",
+                table: "Matchs",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FinishDate",
+                table: "Matchs");
+
+            migrationBuilder.DropColumn(
+                name: "IdWinner",
+                table: "Matchs");
+        }
+    }
+}
--- DatabaseProject/Models/Match.cs
+++ DatabaseProject/Models/Match.cs
@@ -8,12 +8,14 @@ namespace Championship_Login_API.Models
         public string? Name { get; set; }
         public int PhaseNumber { get; set; }
         public DateTime StartDate { get; set; }
-        public Guid IdChampion { get; set; }
+        public DateTime FinishDate { get; set; }
+        public Guid IdChampion { get; set; } //IdChampionship
         public string? Location { get; set; }
         public int TotalTickets { get; set; }
         public Guid IdReferee { get; set; }
         public Guid TeamA { get; set; }
         public Guid TeamB { get; set; }
+        public Guid IdWinner { get; set; }
         public MatchStatusEnum Status { get; set; }
     }
 }
